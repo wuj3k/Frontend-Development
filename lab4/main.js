a27620349effd71e7c39d90c6a81cf0d694e31b6
Patryk <drab.patr@gmail.com>
2019-03-24 08:44:50 +0100

    new file

---
+++ lab4/main.js
@@ -0,0 +1,99 @@
+
+var ship1 = {
+    name: "Majestic Mærsk",
+    inService: true,
+    yardNumber: 4251,
+    imoNumber: 9619919,
+}
+
+var ship2 = {
+    name: "Mary Mærsk",
+    inService: false,
+    yardNumber: 4252,
+    imoNumber: 9619921,
+}
+
+var ship3 = {
+    name: "Marie Mærsk",
+    inService: true,
+    yardNumber: 4253,
+    imoNumber: 9619933,
+}
+
+var ship3 = {
+    name: "Madison Mærsk",
+    inService: false,
+    yardNumber: 4254,
+    imoNumber: 9619945,
+}
+
+var ShipDataBase = (function () {
+    var ships = [];
+
+    function addShip(ship) {
+        if (findShip(ship)) {
+            console.log('ten statek jest juz dodany');
+            return;
+        }
+        ships.push(ship)
+    }
+
+    function removeShip(entryShip) {
+        ships = ships.filter(function (ship) {
+            return ship.name !== entryShip.name;
+        });
+    }
+
+    function updateShip(entryShip) {
+        var key = getKey(entryShip);
+        if (!findShip(entryShip)) {
+            addShip(entryShip);
+            return;
+        };
+        ships[key] = entryShip;
+    }
+
+    function getKey(ship) {
+        return ships.lastIndexOf(ship);
+    }
+
+    function findShip(entryShip, byWhat = 'name', params = '') {
+        var searchBy = params;
+        //if (typeof entryShip.byWhat !== "undefined") return;
+        if (entryShip) searchBy = entryShip.byWhat;
+        if (!searchBy) return;
+
+        return ships.filter(function (ship) {
+            return ship.byWhat == searchBy;
+        });
+    }
+
+    return {
+
+        add: addShip(ship),
+        remove: removeShip(ship),
+        update: updateShip(ship),
+
+        read: function () {
+            console.log(ships);
+        }
+    }
+})();
+
+
+
+
+
+
+add(ship1);
+add(ship2);
+add(ship3);
+remove(ship1);
+console.log(findShip(null, "yardNumber", 4252));
+// ship2.name = "titanic";
+
+// update(ship2);
+
+// console.log(find(ship2, 'yardNumber'))
+
+//read();
