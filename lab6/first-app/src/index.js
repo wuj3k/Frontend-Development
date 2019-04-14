1c44569559153b9b1bc64d194d6b03218cd93ff7
Patryk <drab.patr@gmail.com>
2019-04-14 09:26:20 +0200

    init

--- lab6/first-app/src/index.js
+++ lab6/first-app/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+function Message() {
+    return <h2 className="App-logo">Witaj Janusz!</h2>;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Message />, document.getElementById('root'));
+
